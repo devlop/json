8f679b3c03fa5ccfee6553296f28af4319fc334f
Johan <johan@devlop.se>
2020-12-12 17:23:36 +0700

    add first draft of decode methods

--- src/Json.php
+++ src/Json.php
@@ -4,16 +4,54 @@ declare(strict_types=1);
 
 namespace Devlop\Json;
 
+use JsonException;
+
 final class Json
 {
-
     private function __construct()
     {
-
+        // not meant to be instantiated
     }
 
-    public static function decode(string $json, int $depth = 512) : array
+    /**
+     * Decodes a JSON string
+     *
+     * @link https://www.php.net/manual/en/function.json-decode.php
+     *
+     * @param  string  $json
+     * @param  int  $depth
+     * @return mixed
+     *
+     * @throws JsonException
+     */
+    public static function decode(string $json, int $depth = 512)
     {
-        dd(__METHOD__);
+        return \json_decode(
+            $json,
+            null,
+            $depth,
+            \JSON_THROW_ON_ERROR,
+        );
+    }
+
+    /**
+     * Decodes a JSON string and return JSON objects as associative arrays
+     *
+     * @link https://www.php.net/manual/en/function.json-decode.php
+     *
+     * @param  string  $json
+     * @param  int  $depth
+     * @return mixed
+     *
+     * @throws JsonException
+     */
+    public static function decodeAssoc(string $json, int $depth = 512)
+    {
+        return \json_decode(
+            $json,
+            null,
+            $depth,
+            \JSON_THROW_ON_ERROR | \JSON_OBJECT_AS_ARRAY,
+        );
     }
 }
