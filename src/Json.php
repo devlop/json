04496c9bf78441c7314faefd58d4efbf1e97aadf
Johan <johan@devlop.se>
2021-02-20 02:29:28 +0700

    refactor encoding

--- src/Json.php
+++ src/Json.php
@@ -6,68 +6,117 @@ namespace Devlop\Json;
 
 use JsonException;
 
+/**
+ * Encodes/decodes JSON, always throws on error
+ *
+ * @link https://www.php.net/manual/en/json.constants.php#constant.json-throw-on-error
+ */
 final class Json
 {
+    public const NO_FLAGS = 0;
+
     private function __construct()
     {
         // not meant to be instantiated
     }
 
-    /**
-     * Decodes a JSON string
-     *
-     * @link https://www.php.net/manual/en/function.json-decode.php
-     *
-     * @param  string  $json
-     * @return mixed
-     *
-     * @throws JsonException
-     */
-    public static function decode(string $json)
+    private static function assertCanEncode($value) : void
     {
-        return \json_decode(
-            $json,
-            null,
-            512,
-            \JSON_THROW_ON_ERROR,
-        );
+        if (! is_array($value) && ! is_object($value)) {
+            throw new JsonException(
+                'A value of a type that cannot be encoded was given',
+                \JSON_ERROR_UNSUPPORTED_TYPE
+            );
+        }
     }
 
     /**
-     * Decodes a JSON string and return JSON objects as associative arrays
-     *
-     * @link https://www.php.net/manual/en/function.json-decode.php
-     *
-     * @param  string  $json
-     * @return mixed
-     *
-     * @throws JsonException
-     */
-    public static function decodeAssoc(string $json)
-    {
-        return \json_decode(
-            $json,
-            null,
-            512,
-            \JSON_THROW_ON_ERROR | \JSON_OBJECT_AS_ARRAY,
-        );
-    }
-
-    /**
-     * Returns the JSON representation of a value
+     * Encode as JSON
      *
      * @link https://www.php.net/manual/en/function.json-encode.php
      *
-     * @param  mixed  $value
+     * @param  array|object  $value
+     * @param  int  $flags
+     * @param  int  $depth
      * @return string
      *
      * @throws JsonException
      */
-    public static function encode($value) : string
+    public static function encode($value, int $flags = 0, int $depth = 512) : string
     {
+        self::assertCanEncode($value);
+
         return \json_encode(
             $value,
-            \JSON_THROW_ON_ERROR,
+            $flags | \JSON_THROW_ON_ERROR,
+            $depth,
+        );
+    }
+
+    /**
+     * Encode as JSON and format "pretty print" using whitespace.
+     *
+     * @link https://www.php.net/manual/en/function.json-encode.php
+     * @link https://www.php.net/manual/en/json.constants.php#constant.json-pretty-print
+     *
+     * @param  array|object  $value
+     * @param  int  $flags
+     * @param  int  $depth
+     * @return string
+     *
+     * @throws JsonException
+     */
+    public static function pretty($value, int $flags = 0, int $depth = 512) : string
+    {
+        self::assertCanEncode($value);
+
+        return self::encode(
+            $value,
+            $flags | \JSON_PRETTY_PRINT,
+            $depth,
+        );
+    }
+
+    /**
+     * Decodes a JSON string.
+     *
+     * @link https://www.php.net/manual/en/function.json-decode.php
+     *
+     * @param  string  $json
+     * @param  int  $flags
+     * @param  int  $depth
+     * @return array|object
+     *
+     * @throws JsonException
+     */
+    public static function decode(string $json, int $flags = 0, int $depth = 512)
+    {
+        return \json_decode(
+            $json,
+            null,
+            $depth,
+            $flags | \JSON_THROW_ON_ERROR,
+        );
+    }
+
+    /**
+     * Decodes a JSON string as an associative array.
+     *
+     * @link https://www.php.net/manual/en/function.json-decode.php
+     *
+     * @param  string  $json
+     * @param  int  $flags
+     * @param  int  $depth
+     * @return array|object
+     *
+     * @throws JsonException
+     */
+    public static function decodeAssoc(string $json, int $flags = 0, int $depth = 512)
+    {
+        return self::decode(
+            $json,
+            $flags | \JSON_OBJECT_AS_ARRAY,
+            $depth,
         );
     }
 }
